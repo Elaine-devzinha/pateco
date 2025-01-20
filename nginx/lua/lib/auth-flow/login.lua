87cf1e3e4b303364f1cb47b59d0c9c6a010bfa91
vintalie <marioluizsan@gmail.com>
2025-01-19 23:42:40 -0300

    change server to ngx

--- nginx/lua/lib/auth-flow/login.lua
+++ nginx/lua/lib/auth-flow/login.lua
@@ -22,35 +22,35 @@ function _M.generate_token(id,role)
     return token;
 end
 
-function _M.login(server)
-    local headers = server.req.get_headers()
+function _M.login()
+    local headers = ngx.req.get_headers()
     -- local ct_email = headers["ct_email"]
     -- local pw_usuario = headers["pw_usuario"]
     local ct_email = "danilo@baratao.com"
     local pw_usuario = "764066"
     local valid_credentials = (ct_email ~= nil or ct_email ~= '') and (pw_usuario ~= nil or pw_usuario ~= '')
     if valid_credentials == false then
-        server.status = 403
-        server.say(json.encode({message = "Not Found"}))
-        server.exit(server.OK)
+        ngx.status = 403
+        ngx.say(json.encode({message = "Not Found"}))
+        ngx.exit(ngx.OK)
     end
     if valid_credentials then
         
         local search = db.find_user_by_email(ct_email)
         if (search == nil) or (search == false) then
-            server.status = 404
-            server.say(json.encode({message = "Not Found"}))
-            server.exit(server.OK)
+            ngx.status = 404
+            ngx.say(json.encode({message = "Not Found"}))
+            ngx.exit(ngx.OK)
         end
         if (search.pw_usuario ~= nil) and (bcrypt.verify(pw_usuario, search.pw_usuario)) then
-            server.status = 200
+            ngx.status = 200
             local token = _M.generate_token(search.id, search.nm_grupo)
-            server.say(json.encode({
+            ngx.say(json.encode({
                 message = 'Success loggin',  
                 user_id = search.id, 
                 token = token
             }))
-            server.exit(server.OK)
+            ngx.exit(ngx.OK)
         end
     end
         
