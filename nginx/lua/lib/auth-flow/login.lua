035c86f13e6bdce4b1fab3685db09785c183c91d
vintalie <marioluizsan@gmail.com>
2025-02-17 00:13:29 -0300

    :+1:refactor login function to use request body for credentials and improve validation flow

--- nginx/lua/lib/auth-flow/login.lua
+++ nginx/lua/lib/auth-flow/login.lua
@@ -2,39 +2,40 @@ local _M = {}
 local ngx = require "ngx"
 local db = require("./lib/db")
 local helpers = require("./lib/helpers")
-local bcrypt = require "bcrypt"
 local json = require "cjson"
+
+local bcrypt = require "bcrypt"
 
 
 function _M.login()
-    local headers = ngx.req.get_headers()
-    local ct_email = headers["ct_email"]
-    local pw_usuario = headers["pw_usuario"]
-    local valid_credentials = (ct_email ~= nil or ct_email ~= '') and (pw_usuario ~= nil or pw_usuario ~= '')
-    if valid_credentials == false then
-        helpers.response(ngx.HTTP_FORBIDDEN, "Forbidden")
-    end
-    if valid_credentials then
+    local data = helpers.receive()
+    local ct_email 
+    local pw_usuario 
+    if data ~= nil then
+       
+        ct_email = data.ct_email
+        pw_usuario = data.pw_usuario
         
-        local search = db.find_user_by_email(ct_email)
-
-        if (search == nil) or (search == false) then
-            helpers.response(ngx.HTTP_NOT_FOUND, "Not Found")
+        
+        local valid_credentials = (ct_email ~= nil or ct_email ~= '') and (pw_usuario ~= nil or pw_usuario ~= '')
+        if valid_credentials == false then
+            helpers.response(ngx.HTTP_FORBIDDEN, "Forbidden")
         end
-    
-        if (search ~= nil) and (bcrypt.verify(pw_usuario, search.pw_usuario)) then
+        if valid_credentials then
+            local search = db.find_user_by_email(ct_email) 
+            if (search == nil) or (search == false) then
+                helpers.response(ngx.HTTP_NOT_FOUND, "Not Found")
+            end
+        
+            if (search ~= nil) and (bcrypt.verify(pw_usuario, search.pw_usuario)) then
+                local token = helpers.generate_token(search["id"], search["grupo.nm_grupo"])
 
-            helpers.response(ngx.OK, "Success")
-
-            ngx.status = 200
-            local token = helpers.generate_token(search.id, search.nm_grupo)
-            ngx.say(json.encode({
-                message = 'Success loggin',  
-                user_id = search.id, 
-                token = token
-            }))
-            ngx.exit(ngx.OK)
-        end    
+                helpers.response(ngx.HTTP_OK, "Success loggin", {
+                    user_id = search.id,
+                    token = token
+                })
+            end    
+        end
     end
 
 
